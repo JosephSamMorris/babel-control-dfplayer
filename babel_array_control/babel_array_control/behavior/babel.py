e2df0e8eeb18271d904b33d26bb27cedfd86c979
Owen Trueblood <hi@owentrueblood.com>
2023-04-20 20:28:05 -0400

    Noise for babel behavior

--- babel_array_control/babel_array_control/behavior/babel.py
+++ babel_array_control/babel_array_control/behavior/babel.py
@@ -1,10 +1,32 @@
 from .behavior import Behavior
+from ..lights import unit_index_from_pos, ARRAY_ROWS, ARRAY_COLUMNS
+from .util import constrain
+import perlin
+
+noise = perlin.Perlin(824393)
 
 
 class BabelBehavior(Behavior):
     def __init__(self):
-        super().__init__({})
+        super().__init__({
+            'min_brightness': 0.1,
+            'max_brightness': 0.7,
+        })
 
     def render(self):
-        self.clear_volume(1)
-        self.clear_brightness(1)
+        self.zero_volume()
+        self.zero_brightness()
+
+        # Noise pattern for the light
+        for y in range(ARRAY_ROWS):
+            for x in range(ARRAY_COLUMNS):
+                idx = unit_index_from_pos(x, y)
+
+                if idx is None:
+                    continue
+
+                prl = noise.three(x * 100, y * 300, self.time * 300 + 10)
+                b = (1 - constrain(-prl, 0, 20) / 20) * (self.params['max_brightness'] - self.params['min_brightness']) + self.params['min_brightness']
+
+                self.brightness[idx] = b
+                self.volume[idx] = b
